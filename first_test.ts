a9d96d7cca2a46be569c1ece494e59755c61d0cf
Emircan <canemircan38@gmail.com>
2025-08-06 00:21:23 +0300

    Update first_test.ts

--- first_test.ts
+++ first_test.ts
@@ -1,9 +1,21 @@
-import {test,expect} from '@playwright/test';
+import {test,expect} from '@playwright/test';//test baslama kodu
 
-test('firs test',async({page})=>{
 
-    await page.goto("https://demoblaze.com/");//await bir kodu bekletmeti saglar yani devami var
-    await expect(page).toHaveTitle('STORE');//sayfanin baslik(title) kisminda store yazip yazmanidigini kontrol eder
-    await expect(page).toHaveURL('https://demoblaze.com/');//bu sekil bir wepsitenin olup olmadigini kontrol eder
-}
-)
+test('second_test',async({page})=>
+{
+    await page.goto("https://demoblaze.com/");//sokak girisi gibi dusun
+   
+    await expect(page).toHaveTitle('STORE');//basligin store olup olmadigini incelr
+    await expect(page).toHaveURL('https://demoblaze.com/');//bu ise bu skeilde bir wepsitenin olup olamadigini kontrol eden kod blogu
+
+    await expect(page.locator('h1')).toBeVisible();// tek tirnak içinde yazilan basligin olup olmadigini kontrol eder
+
+    await page.click('text=Login');//login kismini otomatik olarak incelyen bir kod
+
+    await page.fill('#username', 'myUserName');//bu kodda ise kullanici adi ve şifre kismini otomatik olarak test eder (kendi kendi giris yapar yani)
+    await page.fill('#password', 'myPassword123');
+    await page.check('#agree');//Checkbox kutunusu otomatik doldurur
+    await page.selectOption('#country', 'TR');//dil secenegini turkce yapar
+
+
+})
