9b538ed1db4fa3c12b5c746f70b42a46c07bf410
Emircan <canemircan38@gmail.com>
2025-08-06 13:34:20 +0300

    Update homework_test.ts

--- homework_test.ts
+++ homework_test.ts
@@ -1,38 +1,42 @@
 import { test, expect } from '@playwright/test';
-// Import Playwright test functions 'test' and 'expect' for writing tests
+//npx playwright test testhoce.spec.ts --headed ---This method ensures that only the selected file is executed.
 
-// This hook runs before each test
 test.beforeEach(async({page})=>{
-    await page.goto("https://demoblaze.com/");  
-    // Navigate to the website before each test starts
+    await page.goto("https://demoblaze.com/");
 })
 
-test('home test1', async({page})=>{
-    await expect(page).toHaveTitle('STORE');  
-    // Verify the page title is exactly 'STORE'
+test('home test1',async({page})=>{
+
+    await expect(page).toHaveTitle('STORE');
+
 })
 
-test('sign in', async ({ page }) => {
+test('sing in', async ({ page }) => {
   
   await page.click("//a[text()='Sign up']");
-  // Click on the 'Sign up' link using XPath selector
+  await page.fill('#sign-username','thomashelby1021@mail.com');
+  await page.fill('#sign-password',"214421");
+ // await page.click('button.btn.btn-primary');//This has no id
 
-  await page.fill('#sign-username', 'thomashelby1021@mail.com');
-  // Fill the username input field with the email
 
-  await page.fill('#sign-password', "214421");
-  // Fill the password input field with the password
 });
 
-test('log in', async({page})=>{
+
+test('log in',async({page})=>{
 
     await page.click("//a[text()='Log in']");
-    // Click on the 'Log in' link using XPath selector
+    await page.fill('#loginusername','thomashelby1021@mail.com');
+    await page.fill('#loginpassword','214421');
 
-    await page.fill('#loginusername', 'thomashelby1021@mail.com');
-    // Fill the login username input field with the email
 
-    await page.fill('#loginpassword', '214421');
-    // Fill the login password input field with the password
+})
 
+test('search', async ({ page }) => {
+  await expect(page.locator('img')).toBeVisible();
+  await expect(page.locator('h1')).toBeVisible();
+});
+
+//waitt
+test('wait',async({page})=>{
+    await page.waitForSelector('#signin2',{timeout:5000});//
 })
